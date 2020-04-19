82dc6d892e2aa82627a046dead03a52fa03024ed
O4FDev <52675277+O4FDev@users.noreply.github.com>
2020-04-19 20:14:46 +0100

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,73 +1,37 @@
+import speech_recognition as sr
 import os
 from datetime import datetime
 import requests
 import os.path
 
+r = sr.Recognizer()
+m = sr.Microphone()
 
-api_address = 'InsertAPIKey'
-city = "INSERT_CITY"
-blank = "''"
+try:
+    print("A moment of silence, please...")
+    with m as source: r.adjust_for_ambient_noise(source)
+    print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    while True:
+        print("Say something!")
+        with m as source: audio = r.listen(source)
+        print("Got it! Now to recognize it...")
+        try:
+            # recognize speech using Google Speech Recognition
+            value = r.recognize_google(audio)
 
-url = api_address + city
-
-question = input("Hello, how can I help you today?: ")
-if "time" in question:
-    if "date" in question: 
-        now = datetime.now()
-        current_time = now.strftime("%D %H:%M & the seconds are %S")
-        print("")
-        print("Luke the current date & time is: " + current_time)
-        print("")
-    else:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M & the seconds are %S")
-        print("")
-        print("Luke the current time is: " + current_time + ".")
-        print("")
-if "weather" in question: 
-    json_data = requests.get(url).json()
-    formatted_data = json_data['weather'][0]['description']
-    print("")
-    print("Luke, the weather forcast is: " + formatted_data)
-    print("")
-if "skript" in question: 
-    SKName = input("What is the name of your skript going to be?: ")
-    p = ('/Users/lukelucas/Documents/Skripts')
-    completeName = os.path.join(p, SKName+".sk")
-    file1 = open(completeName, "w")
-    SKCommands = input("Do you want to create a command?: ")
-    while SKCommands == "yes":
-        SKCommandsName = input("What's the name of your command?: ")
-        file1.write("command /" + SKCommandsName + ":\n" )
-        SKCommandsPermissions = input("Will there be any permissions for this command?: ")
-        if SKCommandsPermissions == "yes": 
-            SKCommandsPermissionsPermission = input("What is the permission?: ")
-            file1.write("   permission: " + SKCommandsPermissionsPermission + "\n")
-            SKCommandsDescription = input("What is the description of your command?: ")
-            file1.write("   description: " + SKCommandsDescription + "\n")
-            file1.write("   trigger: \n")
-            QuestionClearChat = input("Luke, I noticed the name of your command included clearchat. Do you want me to include the default clear chat command?: ")
-            if QuestionClearChat == "yes": 
-                file1.write("      loop 100 times: \n")
-                file1.write("         broadcast " + blank + "\n")
-                file1.write("      broadcast '&e%player% &fhas cleared the chat.' ")
-            trytoBreakOut = input("Do you want to create another command?: ")
-            if "yes" in trytoBreakOut: 
-                file1.write("\n")
-            else: 
-                break
-        else:
-            SKCommandsDescription = input("What is the description of your command?: ")
-            file1.write("   description: " + SKCommandsDescription + "\n")
-            file1.write("   trigger: \n")
-            if "clearchat" in SKCommandsName: 
-                QuestionClearChat = input("Luke, I noticed the name of your command included clearchat. Do you want me to include the default clear chat command?: ")
-                if QuestionClearChat == "yes": 
-                    file1.write("      loop 100 times: \n")
-                    file1.write("         broadcast " + blank + "\n")
-                    file1.write("      broadcast '&e%player% &fhas cleared the chat.' ")
-            trytoBreakOut = input("Do you want to create another command?: ")
-            if "yes" in trytoBreakOut: 
-                file1.write("\n")
-            else: 
-                break
+            # we need some special handling here to correctly print unicode characters to standard output
+            if str is bytes:  # this version of Python uses bytes for strings (Python 2)
+                if "test" in value: 
+                    now = datetime.now()
+                    current_time = now.strftime("%D %H:%M & the seconds are %S")
+                    print("")
+                    print("Luke the current date & time is: " + current_time)
+                    print("")
+            else:  # this version of Python uses unicode for strings (Python 3+)
+                print("You said {}".format(value))
+        except sr.UnknownValueError:
+            print("Oops! Didn't catch that")
+        except sr.RequestError as e:
+            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+except KeyboardInterrupt:
+    pass
